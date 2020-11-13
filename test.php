47f9f493d1712cb2c9951db73f0370e02e4cff33
Michael Voříšek <mvorisek@mvorisek.cz>
2020-11-13 13:37:41 +0100

    test before build finishes

--- test.php
+++ test.php
@@ -1,7 +1,34 @@
 <?php
 
-if(!extension_loaded('xdebug')) die("no xdebug\n");
+$missingExts = array_diff([
+    'bcmath',
+    'curl',
+    'exif',
+    'gd',
+    'gmp',
+    'igbinary',
+    'imagick',
+    'imap',
+    'intl',
+    'mbstring',
+    'mysqlnd',
+    'openssl',
+    'pdo_mysql',
+    'PDO_OCI',
+    'pdo_pgsql',
+    'pdo_sqlite',
+    ...(PHP_MAJOR_VERSION === 8 ? [] : ['pdo_sqlsrv']), // not available for PHP 8.0 yet
+    'redis',
+    'sockets',
+    'sqlite3',
+    'tidy',
+    'xdebug',
+    'xml',
+    'xsl',
+    'zip',
+], get_loaded_extensions());
 
-die("testing");
-
-echo "ok\n";
+if (count($missingExts) > 0) {
+    echo 'ERROR - missing php extensions: ' . implode(', ', $missingExts) . "\n";
+    exit(1);
+}
