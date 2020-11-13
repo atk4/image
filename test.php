47ce303270783d01bd93f438b03a71f0ea53ca34
Michael Voříšek <mvorisek@mvorisek.cz>
2020-11-13 17:12:39 +0100

    Add common php extensions, integrate tests and remove neovim Alpine pkg (#16)
    * Add bcmath php ext
    
    * Add gmp php ext
    
    * reformat
    
    * fix remove dev packages
    
    * add exif ext
    
    * add xsl ext
    
    * add tidy ext
    
    * add gd ext
    
    * add imagemagick ext
    
    * add redis ext with igbinary
    
    * add zip ext
    
    * adjust php8.0 dockerfile
    
    * remove neovim Alpine pkg
    
    * test before build finishes
    
    * use pickle for install
    
    * no test in Makefile
    
    * add imap ext
    
    * fix test for PHP 7.2
    
    * fix imagick install for PHP 8.0
    
    * add opcache ext

--- test.php
+++ test.php
@@ -1,7 +1,39 @@
 <?php
 
-if(!extension_loaded('xdebug')) die("no xdebug\n");
+$missingExts = array_diff([
+    'bcmath',
+    'curl',
+    'exif',
+    'gd',
+    'gmp',
+    'igbinary',
+    'imagick',
+    'imap',
+    'intl',
+    'mbstring',
+    'mysqlnd',
+    'opcache',
+    'openssl',
+    'pdo_mysql',
+    'PDO_OCI',
+    'pdo_pgsql',
+    'pdo_sqlite',
+    'redis',
+    'sockets',
+    'sqlite3',
+    'tidy',
+    'xdebug',
+    'xml',
+    'xsl',
+    'zip',
+], get_loaded_extensions());
 
-die("testing");
+// TODO remove once pdo_sqlsrv is avaiable for PHP 8.0
+if (PHP_MAJOR_VERSION === 8) {
+    unset($missingExts[array_search('pdo_sqlsrv', $missingExts)]);
+}
 
-echo "ok\n";
+if (count($missingExts) > 0) {
+    echo 'ERROR - missing php extensions: ' . implode(', ', $missingExts) . "\n";
+    exit(1);
+}
