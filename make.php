14cece7d1e807e02e992f84b2e8c67fd3de3a6e0
Michael Voříšek <mvorisek@mvorisek.cz>
2021-02-03 17:11:30 +0100

    Reduce image size - do not install dev packages across steps (#37)
    * Simplify installation pf PHP extensions
    
    * Add xdebug conf file path test
    
    * No "jq" and "npm less clean-css uglify-js"
    
    * reorganize install
    
    * migr xdebug
    
    * migr imagick
    
    * migr redis
    
    * migr gmp
    
    * migr bcmath
    
    * migr everything else
    
    * cleanup
    
    * move "npm" install to full Selenium image
    
    * extra npm image
    
    * cleanup
    
    * install php exts at once
    
    * build also PHP 7.1
    
    * also 5.6 and 7.0
    
    * gen readme
    
    * gen aliases
    
    * no 5.6, 7.0, 7.1
    
    * 7.x, 8.x
    
    * fix readme
    
    Co-authored-by: Michele Locati <michele@locati.it>

--- make.php
+++ make.php
@@ -11,74 +11,90 @@ $cfLabelFromName = function(string $prefix, string $n): string {
     }, $n);
 };
 
+$phpVersions = ['7.2', '7.3', '7.4', '8.0'];
+$imageTypes = [''];
+$targetNames = ['base', 'npm', 'selenium'];
+
+$aliasesPhpVersions = [
+    '7.4' => ['7.x'],
+    '8.0' => ['8.x', 'latest'],
+];
+
+$genImageTags = function(string $imageName) use ($aliasesPhpVersions): array {
+    $res = [$imageName];
+    foreach ($aliasesPhpVersions as $phpVersion => $aliases) {
+        foreach ($aliases as $alias) {
+            $v = preg_replace('~(?<!\d)' . preg_quote($phpVersion, '~') . '(?!\d)~', $alias, $imageName);
+            if ($v !== $imageName) {
+                $res[] = $v;
+            }
+        }
+    }
+
+    return $res;
+};
+
 $imageNames = [];
-foreach ([''] as $imageType) {
-    foreach (['7.2', '7.3', '7.4', '8.0'] as $phpVersion) {
+foreach ($imageTypes as $imageType) {
+    foreach ($phpVersions as $phpVersion) {
         $dockerFile = 'FROM php:' . $phpVersion . '-alpine as base
 
-# install basic PHP
-RUN apk add bash git jq $PHPIZE_DEPS \
-        gmp gmp-dev icu-libs icu-dev libpng libpng-dev imagemagick imagemagick-dev \
-        tidyhtml-libs tidyhtml-dev libxslt libxslt-dev libzip libzip-dev \
-        mysql-client postgresql-client postgresql-dev c-client imap-dev \
-        npm && \
-    docker-php-ext-install bcmath gmp intl exif gd sockets tidy xsl zip mysqli pdo_mysql pdo_pgsql pcntl imap opcache
-RUN wget -q https://github.com/FriendsOfPHP/pickle/releases/latest/download/pickle.phar -O /usr/local/bin/pickle && chmod +x /usr/local/bin/pickle
+# install common PHP extensions
 COPY --from=mlocati/php-extension-installer /usr/bin/install-php-extensions /usr/local/bin/
+RUN install-php-extensions bcmath \
+    exif \
+    gd \
+    gmp \
+    igbinary \
+    imagick \
+    imap \
+    intl \
+    mysqli \
+    opcache \
+    pcntl \
+    pdo_mysql \
+    pdo_oci \
+    pdo_pgsql \
+    pdo_sqlsrv \
+    redis \
+    sockets \
+    tidy \
+    xdebug \
+    xsl \
+    zip
 
-# install basic PECL extensions
-' . (
-$phpVersion < 8
-? 'RUN pickle install imagick && docker-php-ext-enable imagick'
-// @TODO install using pickle once imagick 3.4.5 is released, see https://github.com/Imagick/imagick/releases
-: 'RUN git clone https://github.com/Imagick/imagick && cd imagick \
-    && phpize && ./configure && make all && make install \
-    && echo "extension=imagick.so" >> /usr/local/etc/php/conf.d/docker-php-ext-imagick.ini'
-) . '
-RUN install-php-extensions igbinary
-RUN pickle install redis --no-interaction && docker-php-ext-enable redis
-
-# install xdebug PHP extension
-RUN pecl install xdebug && docker-php-ext-enable xdebug
-
-
-# install Microsoft ODBC drivers & pdo_sqlsrv PHP extension
-RUN install-php-extensions pdo_sqlsrv
-
-
-# install Oracle Instant client & pdo_oci PHP extension
-RUN install-php-extensions pdo_oci
-
-
-# remove build deps
-RUN apk del --purge $PHPIZE_DEPS gmp-dev icu-dev libpng-dev imagemagick-dev \
-        tidyhtml-dev libxslt-dev libzip-dev postgresql-dev imap-dev
-
-
-# install Composer & other tools
+# install Composer
 RUN install-php-extensions @composer
-RUN npm install -g less clean-css uglify-js
 
+# install other tools
+RUN apk add bash git
 
 # run basic tests
 COPY test.php ./
 RUN php test.php && rm test.php
 RUN composer diagnose
 
-FROM base as selenium
+
+FROM base as npm
+
+# install npm
+RUN apk add npm
+
+
+FROM npm as selenium
 
 # install Selenium
-RUN apk add openjdk8-jre-base xvfb ttf-freefont && \
-    curl --fail --silent --show-error -L "https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar" -o /opt/selenium-server-standalone.jar
+RUN apk add openjdk8-jre-base xvfb ttf-freefont \
+    && curl --fail --silent --show-error -L "https://selenium-release.storage.googleapis.com/3.141/selenium-server-standalone-3.141.59.jar" -o /opt/selenium-server-standalone.jar
 
 # install Chrome
 RUN apk add chromium chromium-chromedriver
 
 # install Firefox
-RUN apk add firefox && \
-    curl --fail --silent --show-error -L "https://github.com/mozilla/geckodriver/releases/download/v0.28.0/geckodriver-v0.28.0-linux64.tar.gz" -o /tmp/geckodriver.tar.gz && \
-    tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz && \
-    chmod 755 /opt/geckodriver && ln -s /opt/geckodriver /usr/bin/geckodriver
+RUN apk add firefox \
+    && curl --fail --silent --show-error -L "https://github.com/mozilla/geckodriver/releases/download/v0.28.0/geckodriver-v0.28.0-linux64.tar.gz" -o /tmp/geckodriver.tar.gz \
+    && tar -C /opt -zxf /tmp/geckodriver.tar.gz && rm /tmp/geckodriver.tar.gz \
+    && chmod 755 /opt/geckodriver && ln -s /opt/geckodriver /usr/bin/geckodriver
 ';
 
         $dataDir = __DIR__ . '/data';
@@ -96,15 +112,19 @@ RUN apk add firefox && \
 }
 
 $imageNamesExtended = array_merge(
-    $imageNames,
-    array_map(function($imageName) { return $imageName. '-selenium'; }, $imageNames)
+    ...array_map(function ($targetName) use ($imageNames) {
+        return array_map(function($imageName) use ($targetName) {
+            return $imageName . ($targetName === 'base' ? '' : '-' . $targetName);
+        }, $imageNames);
+    }, $targetNames)
 );
 
 $codefreshFile = 'version: "1.0"
 stages:
   - prepare
-  - build
-  - build_selenium
+' . implode("\n", array_map(function ($targetName) use ($cfLabelFromName) {
+    return '  - ' . $cfLabelFromName('build_', $targetName);
+}, $targetNames)) . '
   - test
   - push
 steps:
@@ -114,33 +134,21 @@ steps:
     repo: atk4/image
     revision: "${{CF_BRANCH}}"
 
-  build:
+' . implode("\n\n", array_map(function ($targetName) use ($imageNames, $cfLabelFromName) {
+    return '  ' . $cfLabelFromName('build_', $targetName) . ':
     type: parallel
-    stage: build
+    stage: ' . $cfLabelFromName('build_', $targetName) . '
     steps:
-' . implode("\n", array_map(function ($imageName) use ($cfLabelFromName) {
-    return '      ' . $cfLabelFromName('b', $imageName) . ':
+' . implode("\n", array_map(function ($imageName) use ($targetName, $cfLabelFromName) {
+        return '      ' . $cfLabelFromName('b', $imageName . ($targetName === 'base' ? '' : '-' . $targetName)) . ':
         type: build
         image_name: atk4/image
-        target: base
-        tag: "${{CF_BUILD_ID}}-' . $imageName . '"
+        target: ' . $targetName . '
+        tag: "${{CF_BUILD_ID}}-' . $imageName . ($targetName === 'base' ? '' : '-' . $targetName) . '"
         registry: atk4
         dockerfile: data/' . $imageName . '/Dockerfile';
-}, $imageNames)) . '
-
-  build_selenium:
-    type: parallel
-    stage: build_selenium
-    steps:
-' . implode("\n", array_map(function ($imageName) use ($cfLabelFromName) {
-        return '      ' . $cfLabelFromName('b', $imageName . '-selenium') . ':
-        type: build
-        image_name: atk4/image
-        target: selenium
-        tag: "${{CF_BUILD_ID}}-' . $imageName . '-selenium"
-        registry: atk4
-        dockerfile: data/' . $imageName . '/Dockerfile';
-    }, $imageNames)) . '
+    }, $imageNames));
+}, $targetNames)) . '
 
   test:
     type: parallel
@@ -162,23 +170,14 @@ steps:
         only:
           - master
     steps:
-' . implode("\n", array_map(function ($imageName) use ($cfLabelFromName) {
-    $res = [];
-    $res[] = '      ' . $cfLabelFromName('p', $imageName) . ':
+' . implode("\n", array_map(function ($imageName) use ($genImageTags, $cfLabelFromName) {
+    return implode("\n", array_map(function ($imageName) use ($cfLabelFromName) {
+        return '      ' . $cfLabelFromName('p', $imageName) . ':
         candidate: "${{' . $cfLabelFromName('b', $imageName) . '}}"
         type: push
         registry: atk4
         tag: "' . $imageName . '"';
-    $imageNameLatest = preg_replace('~(?<!\d)' . preg_quote('8.0', '~') . '(?!\d)~', 'latest', $imageName);
-    if ($imageNameLatest !== $imageName) {
-    $res[] = '      ' . $cfLabelFromName('p', $imageNameLatest) . ':
-        candidate: "${{' . $cfLabelFromName('b', $imageName) . '}}"
-        type: push
-        registry: atk4
-        tag: "' . $imageNameLatest . '"';
-    }
-
-    return implode("\n", $res);
+    }, $genImageTags($imageName)));
 }, $imageNamesExtended)).'
 ';
 file_put_contents(__DIR__ . '/.codefresh/deploy-build-image.yaml', $codefreshFile);
@@ -229,3 +228,22 @@ jobs:
           || docker build -f data/${{ matrix.imageName }}/Dockerfile ./
 ';
 file_put_contents(__DIR__ . '/.github/workflows/ci.yml', $ciFile);
+
+
+$readmeFile = '# Container Images for ATK
+
+<a href="https://g.codefresh.io/public/accounts/romaninsh/pipelines/new/5f6210a9ce7766265b1315f5"><img src="https://g.codefresh.io/api/badges/pipeline/romaninsh/atk4%2Fimage?key=eyJhbGciOiJIUzI1NiJ9.NWRmMjhjZmUxNGEzNzBmNTE4N2JmMjZm.VwoKvoggIuaMYdKMVChMeTX452-jZ5eNfA5t-vO1yXM&date=1600269361326"></a>
+
+This repository builds `atk4/image` image and publishes the following tags:
+
+' . implode("\n", array_map(function ($imageName) use ($genImageTags, $cfLabelFromName) {
+    return '- ' . implode(' ', array_map(function ($imageName) {
+        return '`' . $imageName . '`';
+    }, $genImageTags($imageName)));
+}, $imageNamesExtended)).'
+
+## Running Locally
+
+Run `php make.php` to regenerate Dockerfiles.
+';
+file_put_contents(__DIR__ . '/README.md', $readmeFile);
